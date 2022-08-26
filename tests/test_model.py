47a0d0330a0f21c4faf9c6101a4ab12431d2c4b5
Michael Kihm <Michael.Kihm@rtl.de>
2022-08-26 14:42:18 +0200

    test: update model inference test

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,13 +6,13 @@ from model_training.ml.model import inference
 
 def test_inference(models):
     data = pd.DataFrame({
-        "age": [20],
+        "age": [43],
         "workclass": ["Private"],
-        "education": ["Some-college"],
+        "education": ["11th"],
         "marital-status": ["Married-civ-spouse"],
-        "occupation": ["Exec-managerial"],
+        "occupation": ["Transport-moving"],
         "relationship": ["Husband"],
-        "race": ["Black"],
+        "race": ["White"],
         "sex": ["Male"],
         "hours-per-week": [40],
         "native-country": ["United-States"]
@@ -23,3 +23,4 @@ def test_inference(models):
     y_pred = inference(model, X)
 
     assert len(y_pred) == 1
+    assert y_pred[0] == 1 or y_pred[0] == 0
