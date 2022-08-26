e490c81949487dcd8e732cc3384b52dcc65f284d
Michael Kihm <Michael.Kihm@rtl.de>
2022-08-26 15:37:09 +0200

    feat: inference endpoint

--- api/main.py
+++ api/main.py
@@ -1,8 +1,37 @@
+import pandas as pd
 from fastapi import FastAPI
+
+from model_training.ml.data import CAT_FEATURES, process_data
+from model_training.ml.model import inference, load_models
+
+from .models import InferenceResponse, Person
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!!"}
+    return {"message": "RandomForest inference API for census dataset"}
+
+
+@app.post("/inference", response_model=InferenceResponse)
+async def run_inference(person: Person):
+    data = pd.DataFrame(
+        {
+            "age": [person.age],
+            "workclass": [person.workclass],
+            "education": [person.education],
+            "marital-status": [person.maritalStatus],
+            "occupation": [person.occupation],
+            "relationship": [person.relationship],
+            "race": [person.race],
+            "sex": [person.sex],
+            "hours-per-week": [person.hoursPerWeek],
+            "native-country": [person.nativeCountry],
+        }
+    )
+    model, encoder, lb = load_models()
+    X, *_ = process_data(data, CAT_FEATURES, training=False, encoder=encoder, lb=lb)
+    y_pred = inference(model, X)
+
+    return {"salary": ">50K" if y_pred[0] == 1 else "<=50K"}
