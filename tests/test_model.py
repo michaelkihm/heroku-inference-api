2de56037d8742760e82640b6533bddbf604f789c
Michael Kihm <Michael.Kihm@rtl.de>
2022-08-30 07:35:35 +0200

    test:  write tests for the functions you used to train and evaluate the model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from mock import patch
 from mock_open import MockOpen
+from sklearn.ensemble import RandomForestClassifier
 
 from model_training.ml.data import CAT_FEATURES, process_data
-from model_training.ml.model import data_slice_evaluation, inference
+from model_training.ml.model import (
+    data_slice_evaluation,
+    evaluate_model,
+    inference,
+    train_model,
+)
 
 
 def test_inference_for_salary_lt_50k(models):
@@ -63,3 +69,27 @@ def test_data_slice_evaluation(cleaned_data, models):
         data_slice_evaluation(cleaned_data, model, encoder, lb, CAT_FEATURES)
 
         assert len(f_mock.return_value.write.mock_calls) == expected_calls
+
+
+def test_evaluates_the_model(cleaned_data, models):
+    model, encoder, lb = models
+    precision, recall, fbeta = evaluate_model(
+        cleaned_data, CAT_FEATURES, model, encoder, lb
+    )
+
+    assert 0 <= precision <= 1, "precission should be a value between 0 and 1"
+    assert 0 <= recall <= 1, "recall should be a value between 0 and 1"
+    assert 0 <= fbeta <= 1, "fbeta should be a value between 0 and 1"
+
+
+def test_trains_the_model(cleaned_data, models):
+    # _, encoder, lb = models
+    X_train, y_train, encoder, lb = process_data(
+        cleaned_data, categorical_features=CAT_FEATURES, label="salary", training=True
+    )
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(
+        model, RandomForestClassifier
+    ), "Model should be valid sklean RandomForestClassifier"
